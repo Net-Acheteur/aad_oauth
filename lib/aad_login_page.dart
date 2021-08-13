0a544acea6f16229310ac973e35edcdfcfc6fd2b
Vincent <vderousiers@monchasseurimmo.com>
2021-08-13 22:50:46 +0200

    Customize page

--- lib/aad_login_page.dart
+++ lib/aad_login_page.dart
@@ -9,43 +9,80 @@ class AadLoginPage extends StatefulWidget {
   static String path = 'AadLoginPage';
 
   final RequestCode requestCode = getIt<RequestCode>();
+  final Widget onLoadView;
 
-  AadLoginPage({Key? key}) : super(key: key);
+  AadLoginPage({Key? key, this.onLoadView = const CircularProgressIndicator()}) : super(key: key);
 
   @override
   _AadLoginPageState createState() => _AadLoginPageState();
 }
 
 class _AadLoginPageState extends State<AadLoginPage> {
+  bool exitedWithError = true;
+  bool exited = false;
+  bool pageLoaded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Builder(builder: (BuildContext context) {
-      return WebView(
-        initialUrl: widget.requestCode.getUrlToLaunch(),
-        navigationDelegate: (NavigationRequest request) => NavigationDecision.navigate,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebResourceError: (WebResourceError e) {
-          // HACK for error 102 NSURLErrorCancelled on iOS
-          var iosUrl = e.description.split('NSErrorFailingURLStringKey=');
-          if (iosUrl.length > 1) {
-            var url = iosUrl[1].split(',')[0];
-            widget.requestCode.closeOnUrlChanged(url);
-          }
-          Navigator.of(context).pop();
-        },
-        onPageFinished: (String url) {
-          if (widget.requestCode.closeOnUrlChanged(url)) {
-            Navigator.of(context).pop();
-          }
-        },
-        gestureNavigationEnabled: true,
-      );
+      return Stack(children: [
+        WebView(
+          initialUrl: widget.requestCode.getUrlToLaunch(),
+          navigationDelegate: (NavigationRequest request) => NavigationDecision.navigate,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebResourceError: (WebResourceError e) {
+            // HACK for error 102 NSURLErrorCancelled on iOS
+            var iosUrl = e.description.split('NSErrorFailingURLStringKey=');
+            if (iosUrl.length > 1) {
+              var url = iosUrl[1].split(',')[0];
+              if (widget.requestCode.closeOnUrlChanged(url)) {
+                exitedWithError = false;
+              }
+            }
+            closeView(context);
+          },
+          onProgress: (int progress) {
+            print(progress);
+            if (progress == 100) {
+              setState(() {
+                pageLoaded = true;
+              });
+            }
+          },
+          onPageStarted: (_) {
+            setState(() {
+              pageLoaded = false;
+            });
+          },
+          onPageFinished: (String url) {
+            if (widget.requestCode.closeOnUrlChanged(url)) {
+              exitedWithError = false;
+              closeView(context);
+            }
+          },
+          gestureNavigationEnabled: true,
+        ),
+        !pageLoaded
+            ? Center(
+                child: widget.onLoadView,
+              )
+            : Stack()
+      ]);
     }));
+  }
+
+  void closeView(BuildContext context) {
+    if (!exited) {
+      exited = true;
+      Navigator.of(context).pop();
+    }
   }
 
   @override
   void dispose() {
-    widget.requestCode.closeOnUrlChanged('?error=Close');
+    if (exitedWithError) {
+      widget.requestCode.closeOnUrlChanged('?error=Close');
+    }
     super.dispose();
   }
 }
